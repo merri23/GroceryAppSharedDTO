58b2e47aa8526fbfcd27035c2b420d73259ab4bd
merri23 <mekanhodzaberdiev@gmail.com>
2024-02-24 21:44:01 +0500

    done login and register DTO

---
+++ Sources/GroceryAppSharedDTO/RegisterResponseDTO.swift
@@ -0,0 +1,19 @@
+//
+//  File.swift
+//  
+//
+//  Created by Mekan Hojaberdiyew on 2/24/24.
+//
+
+import Foundation
+
+public struct RegisterResponseDTO: Codable {
+    public let error: Bool
+    public var reason: String? = nil
+    
+    public init(error: Bool, reason: String? = nil) {
+        self.error = error
+        self.reason = reason
+    }
+    
+}
